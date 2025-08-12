6cd720743c150a75abfd6913e5fc62024df58c0f
PavishK <kpavish136@gmail.com>
2025-08-12 18:56:47 +0530

    PixelNoSekai V 1.0 Bug free

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,18 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+import type { NextAuthOptions } from 'next-auth';
+
+export const authOptions : NextAuthOptions={
+    providers:[Google({
+        clientId:process.env.AUTH_CLINET_ID || "",
+        clientSecret:process.env.AUTH_CLIENT_SECRET || "",
+    })],
+    secret:process.env.AUTH_SECRET,
+    session:{
+        strategy:'jwt',
+        maxAge: 3 * 24 * 60 * 60,
+    },
+};
 
 const handler = NextAuth(authOptions);
 
